7620e42a2b7ddac69e261b28b007a336e8dd3eb4
Motozawa <aa8764114@candyhouse.co>
2023-11-24 17:14:30 +0800

    cc

--- main/main.c
+++ main/main.c
@@ -12,13 +12,7 @@ static void ssm_action_handle(sesame * ssm) {
 }
 
 void app_main(void) {
-    ESP_LOGI(TAG, "SesameSDK_ESP32 [11/23][087]");
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) { /* Initialize NVS — it is used to store PHY calibration data */
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    ESP_LOGI(TAG, "SesameSDK_ESP32 [11/24][087]");
     ssm_init(ssm_action_handle);
     esp_ble_init();
 }
